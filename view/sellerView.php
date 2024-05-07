eb793225b3f2682a59011b0e3342adbc87b58108
henrietta jeansson <henriettajeansson@gmail.com>
2024-05-07 13:35:18 +0200

    add output functions to sellers view and update some index.php

--- view/sellerView.php
+++ view/sellerView.php
@@ -11,4 +11,49 @@ class SellerView
         header("Content-Type: application/json");
         echo json_encode($json);
     }
+
+    public function displaySeller(array $seller): void
+    {
+        $json = [
+            'seller-count' => count($seller),
+            'result' => $seller
+        ];
+
+        header("Content-Type: application/json");
+        echo json_encode($json);
+    }
+    public function outputSellerSubmissions(array $seller): void
+    {
+        $json = [
+            "message" => " List of seller's submissions",
+            'seller-count' => count($seller),
+            'result' => $seller
+        ];
+
+        header("Content-Type: application/json");
+        echo json_encode($json);
+    }
+
+    public function outputTotalRevenuePerSeller(array $seller): void
+    {
+        $json = [
+            "message" => " The total revenue of the seller",
+            'seller-count' => count($seller),
+            'result' => $seller
+        ];
+
+        header("Content-Type: application/json");
+        echo json_encode($json);
+    }
+    public function outputSubmittedItemsPerSeller(array $seller): void
+    {
+        $json = [
+            "message" => "Amount of items this seller is selling",
+            'seller-count' => count($seller),
+            'result' => $seller
+        ];
+
+        header("Content-Type: application/json");
+        echo json_encode($json);
+    }
 }
